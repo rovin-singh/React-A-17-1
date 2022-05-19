1aca90003a9b877698c86ac581c8e8b9829b9a3b
rovin-singh <91409265+rovin-singh@users.noreply.github.com>
2022-05-19 13:57:02 +0530

    "changes"

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-
+import TodoList from "react";
 const Todo = () => {
-  const [value, setValue] = React.useState("");
-  console.log(value);
+  const [data, setValue] = React.useState("");
+  const [todos, settodo] = React.useState([]);
+  const addTodo = (newTodo) => {
+    settodo([...todos, { value: newTodo }]);
+  };
+  console.log(todos);
   return (
     <div>
-      <h1>Todo</h1>
+      <h1>TodoList</h1>
       <input
         type="text"
-        placeholder="Enetr Somthing"
+        placeholder="Enter somehing"
         onChange={(e) => setValue(e.target.value)}
       />
-      <button>Add</button>
+      <button
+        onClick={() => {
+          addTodo(data);
+        }}
+      >
+        Add
+      </button>
+      {/*     
+    <TodoList todos1awwwszzzzzzzzzzzzzzzzzzzzzzzzxseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeerftgfvvvvvvtodos}/> */}
     </div>
   );
 };
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TodoList = ({ todos }) => {
+  return (
+    <div>
+      <h1>Rovin SIngh</h1>
+    </div>
+  );
+};
+export default TodoList;
